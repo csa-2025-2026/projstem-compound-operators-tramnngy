819da5f9ca80197898c2091bd56daeebb82a2232
Nhat Tram Nguyen <145398919+tramnngy@users.noreply.github.com>
2025-09-11 21:05:46 +0000

    completed all 3 problems

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,15 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    int truncNum = 678; 
+    System.out.println ( truncNum % 10);
+    truncNum /= 10;
+    System.out.println (truncNum % 10); 
+    truncNum /= 10; 
+    System.out.println ( truncNum % 10);
+    truncNum /= 10;
 
+
+    } 
+ }
--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,15 @@
-
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    int truncNum = 5678; 
+    System.out.println ( truncNum % 10);
+    truncNum /= 10;
+    System.out.println (truncNum % 10);
+    truncNum /= 10; 
+    System.out.println ( truncNum % 10);
+    truncNum /= 10;
+    System.out.println (truncNum % 10);
+    truncNum /= 10;
+  }
+}
